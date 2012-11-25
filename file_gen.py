fcb66301016cc6fe864460465c633b9de55e460f
Kuan-Yu Tseng <user@server>
2012-11-25 11:42:34 -0600

    Add file generator

---
+++ file_gen.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import sys
+if len(sys.argv) < 3:
+	print "Usage: %s <file_name> <file_size(MB)>" % sys.argv[0]
+file = open(sys.argv[1], "w")
+str = 'a'
+for i in range(20):
+	str = str + str;
+for i in range(int(sys.argv[2])):
+	file.write(str)
